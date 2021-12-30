8381057d4b2f11c97be0d2614740822538af09ac
salah <salahukutipuravan@gmail.com>
2021-12-30 20:23:38 +0530

    vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <CardImage></CardImage>
+    <div class="body">
+      <card-body></card-body>
+      <keep-alive>
+        <component v-bind:is="alive" v-on:CardShare="changeTab()" v-on:CardFooter="changeTab()"></component>
+      </keep-alive>
+    </div>
+  </div>
+   <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+        >Frontend Mentor</a
+      >. Coded by <a href="#">Salahudheen</a>.
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CardBody from "./components/CardBody.vue";
+import CardImage from "./components/CardImage.vue";
+import CardFooter from "./components/CardFooter.vue";
+import CardShare from "./components/CardShare.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CardImage,
+    CardBody,
+    CardFooter,
+    CardShare,
+  },
+  data: function () {
+    return {
+      alive: "CardFooter",
+    };
+  },
+  methods:{
+    changeTab(){
+      if(this.alive==="CardFooter"){
+        this.alive="CardShare"
+      }else{
+        this.alive="CardFooter"
+      }
+    }
+  },
+};
+</script>
+<style scoped>
+.container {
+  display: flex;
+  overflow: hidden;
+  margin: auto;
+  border-radius: .6rem;
+}
+.attribution {
+  font-size: 11px;
+  text-align: center;
+  margin-top: 10px;
+}
+.attribution a {
+  color: hsl(228, 45%, 44%);
+}
+@media (max-width: 720px) {
+  .container {
+    flex-direction: column;
+    width: 20rem;
   }
 }
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@media (min-width: 721px) {
+  .container {
+    flex-direction: row;
+    width: 44rem;
+    height: 18rem;
+  }
+  .body {
+    height: 100%;
+    justify-content: end;
+    background-color: white;
+  }
 }
 </style>
